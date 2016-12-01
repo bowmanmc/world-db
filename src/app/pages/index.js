b5e29931e5baa658ebf366e87531dd885483278b
Michael Bowman <bowmanmc@gmail.com>
2016-12-01 03:59:37 -0500

    initial commit

---
+++ src/app/pages/index.js
@@ -0,0 +1,11 @@
+import AboutPage from './AboutPage';
+import Error404Page from './Error404Page';
+import HomePage from './HomePage';
+import FormPage from './FormPage';
+
+export default {
+    AboutPage,
+    Error404Page,
+    HomePage,
+    FormPage
+};
---
+++ tools/startMessage.js
@@ -0,0 +1,32 @@
+import chalk from 'chalk';
+
+// Generated with http://www.text-image.com/convert/ascii.html
+
+const MSG = `
+================================================================================
+============================== World Dashboard =================================
+================================================================================
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMho--/oyhdNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmyNMMMMMMMMMM
+MMMMMMMMMMMMMMs          '.-:/+oossyyyyyyysmNMMMMMMMMMMMMMMMMMMMMMM/ :mMMMMMMMMM
+MMMMMMMMMMMMMN.                              '+mNmMMNMMMMMMMMMMMNds' /hMMMMMMMMM
+MMMMMMMMMMMMN:                               '.-  -sy+dMMMMMMMs'    hMMMMMMMMMMM
+MMMMMMMMMMMMo                                     oM. :hMMMNys.    'hMMMMMMMMMMM
+MMMMMMMMMMMN-                                     oM-  .MMd/     '+dMMMMMMMMMMMM
+MMMMMMMMMMMN                                      .s'  -+.       /MMMMMMMMMMMMMM
+MMMMMMMMMMMM+                                                  '-mMMMMMMMMMMMMMM
+MMMMMMMMMMMMm                                                  'hMMMMMMMMMMMMMMM
+MMMMMMMMMMMMM+                                                  -mMMMMMMMMMMMMMM
+MMMMMMMMMMMMMNo.                                               .yMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMy                                             sNMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMdhy/'                                      :dMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMdo::-:+/                              dMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm:                  -+oss//o. :NMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNo/hh:      :shyhsomMMMMMMMM/ .NMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM/  'yNMMMMMMMMMMMMMMMMN: .NMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo'/MMMMMMMMMMMMMMMMMMMMd+MMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+================================================================================
+`;
+
+console.log(chalk.cyan(MSG));
